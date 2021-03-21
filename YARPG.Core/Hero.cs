d25d5feef95c61a3bb04394541e7fcfeb17d155b
towereater <andrea.n96@hotmail.it>
2021-03-21 23:19:01 +0100

    First commit

---
+++ YARPG.Core/Hero.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace YARPG.Core
+{
+    public class Hero
+    {
+        public int MaxHealth { get; set; }
+        public int CurrentHealth {
+            get => _health;
+            set
+            {
+                if (value < 0)
+                    _health = 0;
+                else
+                    _health = value > MaxHealth ? MaxHealth : value;
+            }
+        }
+        private int _health;
+        public int Damage { get; set; }
+
+        public Hero()
+        {
+
+        }
+    }
+}
---
+++ YARPG_ConsoleGame/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using YARPG.Core;
+
+namespace YARPG_ConsoleGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hero h = new Hero();
+            h.MaxHealth = 15;
+
+            Console.WriteLine("Hello World! MAXH=" + h.MaxHealth);
+        }
+    }
+}
